6436c326490870b14f4e1a9b0186c8766e3bc2c5
Juan Ugalde <juanuu@gmail.com>
2013-09-05 15:56:59 -0700

    Update on the selection analysis scripts

--- SelectionAnalysis_Paml_BranchSite.py
+++ SelectionAnalysis_Paml_BranchSite.py
@@ -338,7 +338,7 @@ if __name__ == '__main__':
             prev_adjusted_pvalue = adjusted_pvalue
             position += 1
 
-            entry.insert(adjusted_pvalue, 4)
+            entry.insert(4, adjusted_pvalue)
 
         corrected_pvalue_file = open(args.output_directory + "/paml_results_corrected.txt", "w")
 
